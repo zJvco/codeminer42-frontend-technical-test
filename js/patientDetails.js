11b0a40a7ee14c74f266e1cd5e83d70f14fa7721
Jvco <65981077+zJvco@users.noreply.github.com>
2021-11-20 01:05:00 -0300

    Adding menu mobile button and create function to load and display the patient info about section 1 on screen

--- js/patientDetails.js
+++ js/patientDetails.js
@@ -1,3 +1,5 @@
+const patientsNav = document.querySelector("nav .patients-list");
+
 // Function to consult API and return a response
 async function consultAPI(endpoint="") {
     url = "https://cm42-medical-dashboard.herokuapp.com/" + endpoint;
@@ -7,12 +9,8 @@ async function consultAPI(endpoint="") {
 
 async function listPatients() {
     const patList = JSON.parse(await (await consultAPI("patients")).text());
-    const aptList = JSON.parse(await (await consultAPI("appointments")).text());
-
-    const nav = document.querySelector("nav");
-
     for (let i = 0; i < patList.length; i++) {
-        nav.innerHTML += `
+        patientsNav.innerHTML += `
         <a href="#" onclick="showDashboard(${patList[i].id})">
             ${patList[i].name}
             <i class="fas fa-angle-right"></i>
@@ -21,11 +19,105 @@ async function listPatients() {
     }
 }
 
+function formatDocument(n) {
+    n = n.split("");
+    let temp = "";
+    for (let i = 0; i < n.length; i++) {
+        if (i == 3 || i == 6) {
+            temp += ".";
+        }
+        else if (i == 9) {
+            temp += "-";
+        }
+        temp += n[i];
+    }
+    return temp;
+}
+
+function formatHealthSystemId(n) {
+    n = n.split("");
+    let temp = "";
+    for (let i = 0; i < n.length; i++) {
+        if (i == 3) {
+            temp += ".";
+        }
+        else if (i == 6) {
+            temp += "/";
+        }
+        temp += n[i];
+    }
+    return temp;
+}
+
+function formatDate(dt) {
+    return `${dt.getFullYear()}/${dt.getMonth()}/${dt.getDate()}`;
+}
+
+function titleCase(txt) {
+    txt = txt.trim().split(" ");
+    let temp = "";
+    for (let i = 0; i < txt.length; i++) {
+        for (let j = 0; j < txt[i].length; j++) {
+            if (j == 0) {
+                temp += txt[i][j].toUpperCase();
+            }
+            else {
+                temp += txt[i][j];
+            }            
+        }
+        if (i + 1 < txt.length) temp += " "; 
+    }
+    return temp;
+}
+
 async function showDashboard(patientId) {
+    patientsNav.classList.remove("show");
+    const contentSections = document.querySelectorAll("main section");
+
+    contentSections.forEach(section => {
+        section.classList.remove("move");
+    });
+
     const singlePatient = JSON.parse(await (await consultAPI(`patients/${patientId}`)).text());
-    
+    const appointmentsList = JSON.parse(await (await consultAPI(`appointments`)).text());
+
+    let lastDate = null;
+    let specialtyName = null;
+    for (let i = 0; i < appointmentsList.length; i++) {
+        if (singlePatient.id == appointmentsList[i].patientId) {
+            if (appointmentsList[i].status == "completed") {
+                if (lastDate != null) {
+                    let tempdt = new Date(appointmentsList[i].endTime);
+                    if (tempdt > lastDate) {
+                        lastDate = tempdt;
+                        specialtyName = appointmentsList[i].specialty;
+                    }
+                }
+                else {
+                    lastDate = new Date(appointmentsList[i].endTime);
+                    specialtyName = appointmentsList[i].specialty;
+                }
+            }
+        }
+    }
+
+    document.getElementById("patient-name").innerText = `${singlePatient.name}`;
+    document.getElementById("patient-doc").innerText = `${formatDocument(singlePatient.document)}`;
+    document.getElementById("patient-insurance-plan").innerText = `${singlePatient.insurancePlan}`;
+    document.getElementById("patient-heath-system-id").innerText = `${formatHealthSystemId(singlePatient.healthSystemId)}`;
+    document.getElementById("last-appointment-specialty").innerText = `${specialtyName == null ? "No Specialty" : titleCase(specialtyName)}`;
+    document.getElementById("last-appointment-date").innerText = `${lastDate == undefined ? "No Date" : formatDate(lastDate)}`;
+
+    contentSections.forEach(section => {
+        section.classList.add("move");
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     listPatients();
+});
+
+const menuBtn = document.querySelector(".nav-menu");
+menuBtn.addEventListener("click", () => {
+    patientsNav.classList.toggle("show");
 });
